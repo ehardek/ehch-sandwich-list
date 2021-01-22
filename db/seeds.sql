a5e40dbedc04f233a0ef3ba5db893a135f08099d
Evan Hardek <evanhardek@gmail.com>
2021-01-21 19:08:31 -0500

    set up Burger_DB and added server.js file

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cola BBQ Bacon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bodacious Bacon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Garbage Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tortilla Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper Burger', true);
+
