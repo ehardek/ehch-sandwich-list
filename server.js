8659d3dea7198c9c6ab7ca575627f8bba5be6121
Evan Hardek <evanhardek@gmail.com>
2021-01-23 14:05:26 -0500

    add server.js functionality aswell as created orm.js file

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const express = require("express")
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static("public"));
+// Parse Application
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Call Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// Set Routes
+var routes = require("./controllers/catsController.js");
+app.use(routes);
+
+app.listen(PORT, function() {
+    //Console check for listen
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
